fc4213587959abfe7a77bb128e5af498badecf77
Aaron Oman <aaron.oman@lendesk.com>
2018-07-11 15:44:33 -0700

    [ch3.1] 4 - variable shadowing

--- 3.1/variables/src/main.rs
+++ 3.1/variables/src/main.rs
@@ -1,10 +1,6 @@
-const MAX_POINTS: u32 = 100_000;
-
 fn main() {
-    let mut x = 5;
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-
-    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
 }
